2b406cfb647d72cce3b894cfcb63d5e80919b342
LongchuanYu <remly@qq.com>
2021-01-10 23:08:36 +0800

    add actions

--- app/utils/actions.py
+++ app/utils/actions.py
@@ -1,14 +1,42 @@
 # coding: utf-8
 action_lib_presets = [
+    # chest
     {
-        'name': 'sover',
+        'name': 'chest_press_machine',
         'label': '坐姿推胸',
         'category': 'chest',
         'values': [{ 'weight': 0, 'numbers': 0 }],
-    }, {
+    },
+    {
+        'name': 'seated_row_machine',
+        'label': '坐姿划船',
+        'category': 'chest',
+        'values': [{ 'weight': 0, 'numbers': 0 }],
+    }
+
+    # leg 
+    {
         'name': 'leg_press',
         'label': '腿部推蹬机',
         'category': 'leg',
         'values': [{ 'weight': 0, 'numbers': 0 }],
-    }
+    },
+    {
+        'name': 'leg_extension',
+        'label': '腿部伸展机',
+        'category': 'leg',
+        'values': [{ 'weight': 0, 'numbers': 0 }],
+    },
+    {
+        'name': 'outer_thigh_abductor_machine',
+        'label': '腿部外弯机',
+        'category': 'leg',
+        'values': [{ 'weight': 0, 'numbers': 0 }],
+    },
+    {
+        'name': 'inner_thigh_adductor_machine',
+        'label': '腿部内弯机',
+        'category': 'leg',
+        'values': [{ 'weight': 0, 'numbers': 0 }],
+    },
 ]
